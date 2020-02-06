6395ac6bc05ba06193222a51e1942cdadc1c3b6b
Giles Bradford <gbradford@zipcodes-MacBook-Pro-4.local>
2020-02-05 21:01:53 -0500

    Completed the TableUtilities.java program. Must go back and review.

--- TableUtilities.java
+++ TableUtilities.java
@@ -1,15 +1,31 @@
- 
+
 
 public class TableUtilities {
     public static String getSmallMultiplicationTable() {
-        return null;
+      String table = "";
+      table = getMultiplicationTable(5);
+        return table;
     }
 
     public static String getLargeMultiplicationTable() {
-        return null;
+        String table = "";
+        table = getMultiplicationTable(10);
+          return table;
     }
 
     public static String getMultiplicationTable(int tableSize) {
-        return null;
+      String table = "";
+      for (int row = 1; row <= tableSize; row++) {
+        for (int column = 1; column <= tableSize; column++) {
+          if (row * column < 10) {
+            table += "  " + column * row + " |";
+          } else if (row * column >= 10 && row * column < 100) {
+            table += " " + column * row + " |";
+          } else {
+            table += row * column + " |";
+          }
+
+        } table += "\n";
+      } return table;
     }
 }
